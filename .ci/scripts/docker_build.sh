72b0a2529458a1f27a46dfcf070a284b0088f5d8
asouto <a.souto@titan-ic.com>
2019-07-31 13:42:38 +0100

    Building image from remote files

--- .ci/scripts/docker_build.sh
+++ .ci/scripts/docker_build.sh
@@ -2,10 +2,13 @@
 
 set -xe
 
-CONTEXT=~/context/
+DOCKER_TAG="${DOCKER_TAG:-latest}"
+
+# CONTEXT=~/context/
 # CONTEXT=.
 PATH_TO_THIS_SCRIPT=$(realpath "$(dirname "$0")")
 DOCKERFILE=$PATH_TO_THIS_SCRIPT/Dockerfile
+CONTEXT=$(realpath "$PATH_TO_THIS_SCRIPT"/../../)
 
-docker build -t suoto/hdlcc:v0.5.1 -f "$DOCKERFILE" "$CONTEXT"
+docker build -t suoto/hdlcc:"$DOCKER_TAG" -f "$DOCKERFILE" "$CONTEXT"
 
